dbf29022f76b4e3960db9cb9848815b8d0366c04
chinaneedM <china.need.grp@gmail.com>
2026-09-19 17:57:40 +0800

    test: cover Korean launch-window disc selection

---
+++ tests/test_stoneage_korean_pc_magazine_launch_window_scan.py
@@ -0,0 +1,22 @@
+import unittest
+
+from tools.stoneage_korean_pc_magazine_launch_window_scan import selected_name
+
+
+class KoreanPcMagazineLaunchWindowScanTests(unittest.TestCase):
+    def test_accepts_dated_launch_window_images(self):
+        self.assertTrue(selected_name("200010/CD1/990915_1453.mdf"))
+        self.assertTrue(selected_name("200103/CD2/FarLand_Tactics.mdf"))
+
+    def test_accepts_legacy_issue_window_images(self):
+        self.assertTrue(selected_name("No.61/CD1/IMAGE.img"))
+        self.assertTrue(selected_name("No.64/CD2/bonus.bin"))
+
+    def test_rejects_outside_window_and_sidecars(self):
+        self.assertFalse(selected_name("200205/CD1/NEW.mdf"))
+        self.assertFalse(selected_name("No.57/CD1/Sin.bin"))
+        self.assertFalse(selected_name("No.61/CD1/IMAGE.ccd"))
+
+
+if __name__=="__main__":
+    unittest.main()
