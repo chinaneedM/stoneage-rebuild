95acefaf211755be58fbef2fcdd064c124416b85
chinaneedM <china.need.grp@gmail.com>
2026-09-19 17:38:58 +0800

    test: cover GameTime KOLIS holding-layer probe

---
+++ tests/test_stoneage_gametime_kolis_holding_probe.py
@@ -0,0 +1,34 @@
+import unittest
+
+from tools.stoneage_gametime_kolis_holding_probe import (
+    FormParser,
+    edition_key,
+    search_form,
+)
+
+
+class GameTimeKolisHoldingProbeTests(unittest.TestCase):
+    def test_edition_key(self):
+        self.assertEqual(
+            edition_key('<a onclick="javascript:fnEdtionList(\'24118251\'); return false;">x</a>'),
+            "24118251",
+        )
+
+    def test_search_form_parser(self):
+        raw='''<form name="searchParamForm" action="/old">
+        <input type="hidden" name="keyword1" value="8995182121">
+        <input type="hidden" name="ufKey" value="">
+        </form>'''
+        attrs,fields=search_form(raw)
+        self.assertEqual(attrs["action"],"/old")
+        self.assertEqual(fields["keyword1"],"8995182121")
+        self.assertEqual(fields["ufKey"],"")
+
+    def test_form_parser_keeps_multiple_forms(self):
+        p=FormParser()
+        p.feed('<form name="x"><input name="a" value="1"></form><form name="searchParamForm"><input name="ufKey" value=""></form>')
+        self.assertEqual(len(p.forms),2)
+
+
+if __name__=="__main__":
+    unittest.main()
