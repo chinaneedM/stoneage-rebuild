d754761500f6bde164bcf4b41953768d523b5d7e
chinaneedM <china.need.grp@gmail.com>
2026-09-19 12:37:20 +0800

    test: update Inium Arquivo query labels

--- tests/test_stoneage_korea2000_arquivopt_probe.py
+++ tests/test_stoneage_korea2000_arquivopt_probe.py
@@ -6,7 +6,8 @@ from tools.stoneage_korea2000_arquivopt_probe import QUERIES, clean
 class Korea2000ArquivoProbeTests(unittest.TestCase):
     def test_queries_cover_three_distribution_surfaces(self):
         labels = {x[0] for x in QUERIES}
-        self.assertIn("inium-url", labels)
+        self.assertIn("inium-url-www", labels)
+        self.assertIn("inium-url-bare", labels)
         self.assertIn("hananet-korean", labels)
         self.assertIn("cnet-korean", labels)
 
