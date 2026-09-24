320b7fd72bdcf573f76ddb2497da686308d137de
chinaneedM <china.need.grp@gmail.com>
2026-09-24 23:31:09 +0800

    test: cover StoneAge 2.5 fingerprint fallbacks

--- tests/test_stoneage_sa25_physical_image_fingerprint_probe.py
+++ tests/test_stoneage_sa25_physical_image_fingerprint_probe.py
@@ -3,6 +3,7 @@ import unittest
 from tools.stoneage_sa25_physical_image_fingerprint_probe import (
     ImageParser,
     hamming_hex,
+    insecure_tls_allowed,
     is_thumbnail,
     normalize_image_url,
 )
@@ -25,6 +26,12 @@ class PhysicalImageFingerprintProbeTests(unittest.TestCase):
             normalize_image_url("https://example.com/p/q.html", "../a.jpg"),
             "https://example.com/a.jpg",
         )
+
+    def test_tls_exception_is_host_limited(self):
+        self.assertTrue(insecure_tls_allowed("https://www.shiqi.me/pt_17.htm"))
+        self.assertTrue(insecure_tls_allowed("https://shiqi.me/x"))
+        self.assertFalse(insecure_tls_allowed("https://a.rimg.com.tw/x.jpg"))
+        self.assertFalse(insecure_tls_allowed("https://example.com/x"))
 
     def test_hamming(self):
         self.assertEqual(hamming_hex("0000000000000000", "0000000000000000"), 0)
