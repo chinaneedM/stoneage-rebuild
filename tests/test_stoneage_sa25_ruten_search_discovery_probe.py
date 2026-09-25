888832129aac23939df8b31ad3ba2411c309b221
chinaneedM <china.need.grp@gmail.com>
2026-09-25 12:01:39 +0800

    test: cover Ruten StoneAge 2.5 listing discovery

---
+++ tests/test_stoneage_sa25_ruten_search_discovery_probe.py
@@ -0,0 +1,18 @@
+import unittest
+from tools.stoneage_sa25_ruten_search_discovery_probe import ids_from_html, relevant_contexts
+
+class T(unittest.TestCase):
+    def test_extract_ids(self):
+        s='''<a href="https://www.ruten.com.tw/item/22445165101247/">x</a>
+        <a href="/item/22242541948520/">y</a>'''
+        self.assertEqual(ids_from_html(s),("22445165101247","22242541948520"))
+
+    def test_relevant_context(self):
+        s='''<div>石器時代 2.5版 精靈王傳說
+        <a href="/item/22445165101247/">全新完整版</a></div>'''
+        rows=relevant_contexts(s,ids_from_html(s))
+        self.assertEqual(len(rows),1)
+        self.assertEqual(rows[0][0],"22445165101247")
+
+if __name__=="__main__":
+    unittest.main()
