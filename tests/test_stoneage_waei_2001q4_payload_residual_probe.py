92de224d443834fe5a7f9916906b2a9f65942736
chinaneedM <china.need.grp@gmail.com>
2026-09-25 20:20:30 +0800

    test: reject generic Waei download false positives

--- tests/test_stoneage_waei_2001q4_payload_residual_probe.py
+++ tests/test_stoneage_waei_2001q4_payload_residual_probe.py
@@ -1,5 +1,5 @@
 import unittest
-from tools.stoneage_waei_2001q4_payload_residual_probe import WINDOWS,score
+from tools.stoneage_waei_2001q4_payload_residual_probe import WINDOWS,score,stoneage_marker
 
 class Waei2001Q4PayloadResidualTests(unittest.TestCase):
     def test_residual_includes_november_exe(self):
@@ -13,6 +13,14 @@ class Waei2001Q4PayloadResidualTests(unittest.TestCase):
     def test_scoring_exact_setup(self):
         self.assertGreater(score("http://www.waei.com.cn/a/stoneage2.0setup.exe"),20)
         self.assertEqual(score("http://www.waei.com.cn/a/logo.gif"),0)
+
+    def test_generic_other_game_download_is_not_stoneage(self):
+        u="http://www.waei.com.cn/zhuanqu/t4c/download/screen-saver/Setup_800.exe"
+        self.assertFalse(stoneage_marker(u))
+        self.assertEqual(score(u),0)
+
+    def test_sa20_filename_can_remain_discovery_marker(self):
+        self.assertTrue(stoneage_marker("http://www.waei.com.cn/down/sa20client.exe"))
 
 if __name__=="__main__":
     unittest.main()
