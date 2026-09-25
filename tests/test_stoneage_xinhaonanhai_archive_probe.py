adbec41bf5acba8cecef24cf327da3fd0d291618
chinaneedM <china.need.grp@gmail.com>
2026-09-25 16:56:59 +0800

    test: cover 2000 xinhaonanhai map target

--- tests/test_stoneage_xinhaonanhai_archive_probe.py
+++ tests/test_stoneage_xinhaonanhai_archive_probe.py
@@ -3,6 +3,8 @@ from tools.stoneage_xinhaonanhai_archive_probe import relevant_url
 class ContributorProbeTests(unittest.TestCase):
     def test_target_relevant(self):
         self.assertTrue(relevant_url("http://www.xinhaonanhai.com/down/Estoneage2.0map_1127.exe"))
+    def test_2000_map_target(self):
+        self.assertTrue(relevant_url("http://www.xinhaonanhai.com/down/samap_1220.zip"))
     def test_stoneage_zip_relevant(self):
         self.assertTrue(relevant_url("http://www.xinhaonanhai.com/stoneage/map.zip"))
     def test_unrelated_false(self):
